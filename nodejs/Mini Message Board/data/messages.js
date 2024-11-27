2f5a1e8c33774b2704287a6253f33494f0b480e3
Osama Rabea <osrab3@gmail.com>
2024-11-28 01:29:59 +0200

    data

---
+++ nodejs/Mini Message Board/data/messages.js
@@ -0,0 +1,21 @@
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "Hi everyone !",
+        user: "Alex",
+        added: new Date()
+    },
+];
+
+
+module.exports = messages;
